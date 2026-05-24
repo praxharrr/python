6bc2e568cfdeabd2ef53401014874d28cb24c3ed
Prakhar Sharma <praxhar001@gmail.com>
2026-05-24 23:17:58 +0530

    Add files via upload

---
+++ 01_duplication.py
@@ -0,0 +1,9 @@
+def print_order(name, chai_type):
+    print(f"{name} orderded {chai_type} chai!")
+
+
+print_order("Aman", "masala")
+print_order("Hitesh", "Ginger")
+print_order("Jia", "Tulsi")
+print_order("pucchu" , "meetha")
+
